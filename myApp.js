577257501124d1cd497d5d251dc154d5a5090ffa
Wesley Kelly <wesley.kelly@hotmail.com>
2022-02-09 19:07:10 +0000

    setup?

--- myApp.js
+++ myApp.js
@@ -1,5 +1,19 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected to db")
+});
+
+
+
+const kittySchema = new mongoose.Schema({
+  name: String
+});
 
 let Person;
 
