fc90ce5d8ee49b8a9ab39cf89986377535f24a3b
Wesley Kelly <wesley.kelly@hotmail.com>
2022-02-19 16:29:51 +0000

    task6

--- myApp.js
+++ myApp.js
@@ -53,7 +53,13 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+ 
+  Person.findOne({favoriteFoods: food}, function(err, personFound ) {
+  if(err)return console.error(err)
+  done(null, personFound)
+ })
+ 
+ 
 };
 
 const findPersonById = (personId, done) => {
