5cffd702cc35baef9785673870266be12ba60c3a
Wesley Kelly <wesley.kelly@hotmail.com>
2022-02-24 17:18:03 +0000

    task 8

--- myApp.js
+++ myApp.js
@@ -95,7 +95,7 @@ done(null, updatedPerson)
 };
 
 const removeById = (personId, done) => {
- Person.findByIdAndDelete({id:personId},(err, personToDelete)=>{
+ Person.findByIdAndRemove({id:personId},(err, personToDelete)=>{
   if(err) return console.log(err);
  done(null ,personToDelete);
  })
