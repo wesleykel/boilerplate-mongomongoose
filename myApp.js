db3ce7beafe87232742047be81a45892c530276a
Wesley Kelly <wesley.kelly@hotmail.com>
2022-02-24 17:29:01 +0000

    task 8

--- myApp.js
+++ myApp.js
@@ -105,8 +105,11 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
+Person.remove({name:nameToRemove},(err, response)=>{
+  if(err) return console.log(err);
+  done(null , response);
+})
 
-  done(null /*, data*/);
 };
 
 const queryChain = (done) => {
